f178b90868908a8a4b8ae970b5ea750aa64203a0
Mikael Fredriksson <micke@sictransit.net>
2022-11-23 09:03:35 +0100

    "Hello, World!"

---
+++ Davvisámegillii/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Davvisámegillii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
