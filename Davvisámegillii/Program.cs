3efbaa9f4ee8735d3747219b157a98241aa9d8dd
Mikael Fredriksson <micke@sictransit.net>
2022-11-26 18:41:21 +0100

    UTF8 console output is kind of important

--- Davvisámegillii/Program.cs
+++ Davvisámegillii/Program.cs
@@ -4,6 +4,8 @@
     {
         public static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             for (var i = 0; i <= 1000; i++)
             {
                 Console.WriteLine($"{i}: {i.ToNumeral()} {i.ToNumeral(true)}");
