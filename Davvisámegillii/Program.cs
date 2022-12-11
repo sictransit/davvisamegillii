5157b2e23fa6c0235bdad388f466b49af69e71f1
Mikael Fredriksson <micke@sictransit.net>
2022-12-11 10:45:55 +0100

    telling time

--- Davvisámegillii/Program.cs
+++ Davvisámegillii/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Davvisámegillii
+﻿using System.Globalization;
+
+namespace Davvisámegillii
 {
     public static class Program
     {
@@ -6,7 +8,7 @@
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            for (var i = 0; i <= 1000; i++)
+            for (var i = 0; i <= 100; i++)
             {
                 Console.WriteLine($"{i}: {i.ToNumeral()} {i.ToNumeral(true)}");
             }
@@ -14,6 +16,12 @@
             for (var i = -6; i < 7; i++)
             {
                 Console.WriteLine($"{DateTime.Today.AddDays(i):yyyy-MM-dd}: {DateTime.Today.AddDays(i).ToDate()}");
+            }
+
+            for (var m = 0; m < 1440; m++)
+            {
+                var t = DateTime.Now.AddMinutes(m);
+                Console.WriteLine($"{t.ToString("HH:mm", CultureInfo.InvariantCulture)}: diibmu lea {t.TimeOfDay.ToTime()}");
             }
         }
     }
